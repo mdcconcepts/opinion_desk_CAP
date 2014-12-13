97b8524e52fb11e77b35b9c2ebff37885fbd5861
code-lord <vikrambghadge@gmail.com>
2014-12-13 12:34:49 -0800

    initi commit 13-12-2014

--- protected/models/CustomerCustomField.php
+++ protected/models/CustomerCustomField.php
@@ -10,147 +10,140 @@
  * @property integer $field_maxsize
  * @property integer $lob_id
  */
-class CustomerCustomField extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'customer_custom_field';
-	}
+class CustomerCustomField extends CActiveRecord {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('field_name, field_category_id, field_maxsize, lob_id', 'required'),
-			array('field_category_id, field_maxsize, lob_id', 'numerical', 'integerOnly'=>true),
-			array('field_name', 'length', 'max'=>45),
-			/*
-			//Example username
-			array('username', 'match', 'pattern' => '/^[A-Za-z0-9_]+$/u',
-                 'message'=>'Username can contain only alphanumeric 
-                             characters and hyphens(-).'),
-          	array('username','unique'),
-          	*/
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, field_name, field_category_id, field_maxsize, lob_id', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'customer_custom_field';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('field_name, field_category_id, field_maxsize, lob_id', 'required'),
+            array('field_category_id, field_maxsize, lob_id', 'numerical', 'integerOnly' => true),
+            array('field_name', 'length', 'max' => 45),
+            /*
+              //Example username
+              array('username', 'match', 'pattern' => '/^[A-Za-z0-9_]+$/u',
+              'message'=>'Username can contain only alphanumeric
+              characters and hyphens(-).'),
+              array('username','unique'),
+             */
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, field_name, field_category_id, field_maxsize, lob_id', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'field_name' => 'Field Name',
-			'field_category_id' => 'Field Category',
-			'field_maxsize' => 'Field Maxsize',
-			'lob_id' => 'Lob',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'field_name' => 'Field Name',
+            'field_category_id' => 'Field Category',
+            'field_maxsize' => 'Field Maxsize',
+            'lob_id' => 'Lob',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('field_name',$this->field_name,true);
-		$criteria->compare('field_category_id',$this->field_category_id);
-		$criteria->compare('field_maxsize',$this->field_maxsize);
-		$criteria->compare('lob_id',$this->lob_id);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id);
+        $criteria->compare('field_name', $this->field_name, true);
+        $criteria->compare('field_category_id', $this->field_category_id);
+        $criteria->compare('field_maxsize', $this->field_maxsize);
+        $criteria->compare('lob_id', $this->lob_id);
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return CustomerCustomField the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
-	
-	public function beforeSave() 
-    {
-        $userId=0;
-		if(null!=Yii::app()->user->id) $userId=(int)Yii::app()->user->id;
-		
-		if($this->isNewRecord)
-        {           
-                        						
-        }else{
-                        						
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return CustomerCustomField the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    public function beforeSave() {
+        $userId = 0;
+        if (null != Yii::app()->user->id)
+            $userId = (int) Yii::app()->user->id;
+
+        if ($this->isNewRecord) {
+            
+        } else {
+            
         }
 
-        
+
         return parent::beforeSave();
     }
 
-    public function beforeDelete () {
-		$userId=0;
-		if(null!=Yii::app()->user->id) $userId=(int)Yii::app()->user->id;
-                                
+    public function beforeDelete() {
+        $userId = 0;
+        if (null != Yii::app()->user->id)
+            $userId = (int) Yii::app()->user->id;
+
         return false;
     }
 
-    public function afterFind()    {
-         
+    public function afterFind() {
+
         parent::afterFind();
     }
-	
-		
-	public function defaultScope()
-    {
-    	/*
-    	//Example Scope
-    	return array(
-	        'condition'=>"deleted IS NULL ",
-            'order'=>'create_time DESC',
-            'limit'=>5,
-        );
-        */
-        $scope=array();
 
-        
+    public function defaultScope() {
+        /*
+          //Example Scope
+          return array(
+          'condition'=>"deleted IS NULL ",
+          'order'=>'create_time DESC',
+          'limit'=>5,
+          );
+         */
+        $scope = array();
+
+
         return $scope;
     }
+
 }
